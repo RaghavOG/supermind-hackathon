a63a0456ac33edc416d62e2efb6ae67f38a4ab4c
RaghavOG <04raghavsingla28@gmail.com>
2025-01-04 18:50:24 +0530

    fix: Updates From Anubhav

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+    return(
+        <div style={{background: 'url(https://wallpaperaccess.com/full/1764254.jpg) no-repeat center center', backgroundSize: 'cover'}} className="h-screen flex items-center justify-center">
+            <div >
+                <h1 className="text-8xl text-white font-bold">
+                    Welcome text here
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quae ut esse error accusamus quo iure earum labore cupiditate repellendus tempora voluptatem vero, ipsam modi praesentium odit est. Ut, ex.
+                </p>
+            </div>
+            <div className="">
+
+            </div>
+    </div>
+    )
+}
